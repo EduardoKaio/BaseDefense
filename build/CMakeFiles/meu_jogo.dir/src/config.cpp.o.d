4362612b8243830b658174be6e533e054bbd4765
felipe <felipefreitas13@live.com>
2024-08-14 02:36:23 -0300

    test: teste do gitignore

--- build/CMakeFiles/meu_jogo.dir/src/config.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/meu_jogo.dir/src/config.cpp.o: \
- /home/kaio/Documentos/UFRN/lp/BaseDefense/src/config.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/meu_jogo.dir/src/municao.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/meu_jogo.dir/src/municao.cpp.o: \
- /home/kaio/Documentos/UFRN/lp/BaseDefense/src/municao.cpp \
- /usr/include/stdc-predef.h
